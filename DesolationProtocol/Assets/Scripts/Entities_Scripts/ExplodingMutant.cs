307c04f0cc83e3a3612a39adc90d1a342e4a80ad
TomasRacciatti <tomasracciatti@gmail.com>
2024-05-04 18:35:26 -0300

    Enemigo Explosivo Terminado
    Saca mucho daño al jugador o entities a su alrededor, no hace daño a traves de las paredes. Explota si se acerca o si su vida llega a 0

--- DesolationProtocol/Assets/Scripts/Entities_Scripts/ExplodingMutant.cs
+++ DesolationProtocol/Assets/Scripts/Entities_Scripts/ExplodingMutant.cs
@@ -8,6 +8,7 @@ public class ExplodingMutant : MonoBehaviour
 
     [SerializeField] private NavMeshAgent _agent;
     public Transform Player;
+    public Transform Entity;
     public LayerMask WhatIsPlayer;
     private bool _isExploding = false;
     [SerializeField] private float _attackRange;
@@ -24,17 +25,19 @@ public class ExplodingMutant : MonoBehaviour
     private AudioSource _audioSource;
 
     [SerializeField] private float _explosionRange;
-    [SerializeField] private int _maxHits;
+    [SerializeField] private int _maxHits = 30;
     [SerializeField] private int _maxDamage = 71;
     [SerializeField] private int _minDamage = 30;
 
-    public LayerMask HitLayer, BlockExplosionsLayer;
+    public LayerMask HitLayer;
+    public LayerMask BlockExplosionsLayer;
 
-    private Collider[] _hits;
+    private Collider[] _entitiesHit;
 
     private void Awake()
     {
         Player = FindObjectOfType<ScPlayer>().transform;
+        Entity = FindObjectOfType<ScEntity>().transform;
         _agent = GetComponent<NavMeshAgent>();
 
         _anim = GetComponentInChildren<Animator>();
@@ -44,7 +47,7 @@ public class ExplodingMutant : MonoBehaviour
 
         _audioSource = GetComponent<AudioSource>();
 
-        _hits = new Collider [_maxHits];
+        _entitiesHit = new Collider[_maxHits];
     }
 
     private void Update()
@@ -77,7 +80,7 @@ public class ExplodingMutant : MonoBehaviour
     {
 
         _healthPoints -= incomingDamage;
-        
+
         //Validate Death
         if (_healthPoints <= 0)
         {
@@ -92,8 +95,36 @@ public class ExplodingMutant : MonoBehaviour
 
         _audioSource.Play();
 
-        Collider[] entitiesInRange = Physics.OverlapSphere(transform.position, _explosionRange);
 
+        // No Pega a traves de las paredes
+
+        Ray _explosionRay = new Ray(transform.position, Entity.position);
+        
+        if (Physics.Raycast(_explosionRay, out RaycastHit hit))
+        {
+            int _totalHits = Physics.OverlapSphereNonAlloc(transform.position, _explosionRange, _entitiesHit, HitLayer);
+
+
+            for (int i = 0; i < _totalHits; i++)
+            {
+                if (_entitiesHit[i].GetComponent<ScEntity>())
+                {
+                    float distance = Vector3.Distance(hit.point, _entitiesHit[i].transform.position);
+
+                    if (!Physics.Raycast(hit.point, (_entitiesHit[i].transform.position - hit.point).normalized, distance, BlockExplosionsLayer.value))
+                    {
+                        _entitiesHit[i].GetComponent<ScEntity>().TakeDamage(Random.Range(_minDamage, _maxDamage));
+                        Debug.Log("Take Damage");
+                    }
+                }
+            }
+        }
+        
+
+
+        //Funciona pero tambien golpea a traves de las paredes - OBSOLETO
+        /*
+        Collider[] entitiesInRange = Physics.OverlapSphere(transform.position, _explosionRange);
         foreach (Collider entity in entitiesInRange)
         {
             if (entity.GetComponent<ScEntity>() != null)
@@ -102,6 +133,7 @@ public class ExplodingMutant : MonoBehaviour
             }
 
         }
+        */
     }
 
 
